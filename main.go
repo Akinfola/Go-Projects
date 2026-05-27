50961989f201d4d4f8dc174d0903010b71f0e7a0
Akinfola <ejideayodele@gmail.com>
2026-05-28 00:17:48 +0100

    feat: Replace Println with Printf for formatted output and add ticket booking tracking

--- main.go
+++ main.go
@@ -3,8 +3,38 @@ package main
 import "fmt"
 
 func main() {
+	//declare variables and constants
 	var conferenceName = "Go Conference"
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("Get your tickets to attend")
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	//Welcome message, value of conferenceName and conferenceTickets are used in the message using the Println function
+	// fmt.Println("Welcome to", conferenceName, "booking application")
+	// fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	// fmt.Println("Get your tickets to attend")
+
+	// // Data types using the Println function
+	// var userName string
+	// var userTickets int
+
+	// //Ask user for their name
+	// userName = "Tom"
+	// userTickets = 2
+	// fmt.Println(userName)
+	// fmt.Println(userTickets)
+
+	//Welcome message, value of conferenceName and conferenceTickets are used in the message using the Printf function
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets to attend\n")
+
+	// Data types using the Printf function
+	var userName string
+	var userTickets int
+
+	//Ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
